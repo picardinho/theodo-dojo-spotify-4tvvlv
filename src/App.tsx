f9a9f2556ee32760965e2ebbffea46615f737631
picardinho <131191449+picardinho@users.noreply.github.com>
2023-04-18 19:20:06 +0200

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">la kiffance</h1>
+        <h1 className="App-title">la kifface</h1>
       </header>
       <div className="App-images">
         <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
