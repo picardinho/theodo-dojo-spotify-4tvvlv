d51738ef215614ae597a0fcbe1b60a985da1416e
picardinho <131191449+picardinho@users.noreply.github.com>
2023-04-18 19:22:15 +0200

    musique mise

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import logo from './assets/logo.svg';
 import './App.css';
-<audio src={trackUrls[1]} autoPlay controls />
+
 
 const App = () => {
   return (
     <div className="App">
+      <audio src={trackUrls[1]} autoPlay controls />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">la kiffance</h1>
